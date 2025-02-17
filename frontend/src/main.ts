9f8bef0a1c544e6c509999b0f66a223ec0732d27
TianYi <13518656+tianyi_za@user.noreply.gitee.com>
2025-02-17 20:23:45 +0800

    feat:UI init

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+//createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(ElementPlus);
+app.mount('#app');
---
+++ frontend/src/views/login.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="login-container">
+        <h2>登录</h2>
+        <form @submit.prevent="handleLogin">
+            
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const handleLogin = () => {
+
+};
+</script>
+
+<style scoped>
+
+</style>
