3a03083b013af790e2cf35f53fca58e5e534bd0c
TianYi <142967939+ztygod@users.noreply.github.com>
2025-02-21 16:47:35 +0800

    feat:create task moudle

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,41 @@
 import { createRouter, createMemoryHistory } from "vue-router";
-import home from "../views/home.vue";
-import login from "../views/login.vue";
+import Signboard from "../views/signboard.vue";
+import Report from "../views/report.vue";
+import Task from "../views/task.vue";
+import Home from "../views/home.vue";
+import Login from "../views/login.vue";
 
 const routes = [
     {
         path: '/',
         name: 'home',
-        component: home,
-        meta: { requiresAuth: true } // 标记需要认证的页面
+        component: Home,
+        meta: { requiresAuth: true },// 标记需要认证的页面
+        children: [
+            {
+                path: 'task',
+                name: 'task',
+                component: Task,
+                meta: { requiresAuth: true },// 标记需要认证的页面
+            },
+            {
+                path: 'report',
+                name: "report",
+                component: Report,
+                meta: { requiresAuth: true },// 标记需要认证的页面
+            },
+            {
+                path: 'signboard',
+                name: 'signboard',
+                component: Signboard,
+                meta: { requiresAuth: true },// 标记需要认证的页面
+            }
+        ]
     },
     {
         path: '/login',
         name: 'login',
-        component: login,
+        component: Login,
     }
 ];
 
