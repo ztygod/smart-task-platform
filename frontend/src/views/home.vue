b53c62afa9ddc0f3303836c35a2f722f7b30888a
TianYi <142967939+ztygod@users.noreply.github.com>
2025-02-23 15:00:27 +0800

    feat:sidebar

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -21,6 +21,31 @@
                 <span>◁</span>
             </button>
         </header>
+
+        <main>
+            <nav>
+                <ul>
+                    <li>
+                        <router-link to="/home/task" active-class="active" class="router">
+                            <el-icon size="30" color="white"><House /></el-icon>
+                            <span v-if="!homeDate.isCollapsed" class="link-text">任务</span>
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link to="/home/signboard" active-class="active" class="router">
+                            <el-icon size="30" color="white"><Notification /></el-icon>
+                            <span v-if="!homeDate.isCollapsed" class="link-text">看板</span>
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link to="/home/report" active-class="active" class="router">
+                            <el-icon size="30" color="white"><DocumentRemove /></el-icon>
+                            <span v-if="!homeDate.isCollapsed" class="link-text">每日汇报</span>
+                        </router-link>
+                    </li>
+                </ul>
+            </nav>
+        </main>
     </div>    
 </template>
 
@@ -41,8 +66,8 @@ const collapsedSidebar = () => {
 .sidebar{
     width: 250px;
     height: 100vh;
-    background-color: #2c3e50;
-    color: white;
+    background-color: #b6c4da;
+    color: black;
     transition: width 0.3s ease;
     display: flex;
     flex-direction: column;
@@ -51,7 +76,7 @@ const collapsedSidebar = () => {
   width: 80px;
 }
 .sidebar-header{
-    height: 100px;
+    height: 80px;
     display: flex;
     flex-direction: row;
     gap: 80px;
@@ -68,5 +93,42 @@ const collapsedSidebar = () => {
     height: 50px;
     margin: 10px 0 0 0;
     border-radius: 25px;
+}
+nav {
+  flex-grow: 1;
+  overflow-y: auto;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  padding: 15px;
+  transition: background-color 0.2s ease;
+}
+
+li:hover {
+  background-color: #bfdcf1;
+}
+.router-link-active, .router-link-exact-active {
+  text-decoration: none;
+}
+.active {
+  background-color: #38a6f9;
+}
+.router{
+    text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    height: 40px;
+    border-radius: 10px;
+}
+.link-text{
+    color: white;
+    text-align: center;
+    line-height: 35px;
 }
 </style>
