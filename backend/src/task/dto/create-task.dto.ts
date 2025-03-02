8660de36addd1b6fc9e9877f93d912d5b327011b
TianYi <142967939+ztygod@users.noreply.github.com>
2025-03-02 15:55:50 +0800

    feat:add NLP

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -1,16 +1,16 @@
-import { IsEmpty } from "class-validator";
+import { IsNotEmpty } from "class-validator";
 import { TaskStatus } from "../task.interface";
 
 export class CreateTaskDto {
-    @IsEmpty({ message: '名称不能为空' })
+    @IsNotEmpty({ message: '名称不能为空' })
     title: string
 
-    @IsEmpty({ message: '描述不能为空' })
+    @IsNotEmpty({ message: '描述不能为空' })
     description: string
 
-    @IsEmpty({ message: '状态不能为空' })
+    @IsNotEmpty({ message: '状态不能为空' })
     status: TaskStatus
 
-    @IsEmpty({ message: '预期完成时间不能为空' })
+    @IsNotEmpty({ message: '预期完成时间不能为空' })
     due_date: Date
 }
